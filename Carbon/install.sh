815d457906976d33997d0f1fa163ca3bf52ee562
c4droid <c4droid@foxmail.com>
2018-05-01 18:23:26 +0800

    Rebuild code

---
+++ Carbon/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+ROOT=`pwd`
+BUILD=Build/Build
+MOUNT=/mnt/Carbon
+GRUB=grub.img
+TARGET=$ROOT/Carbon.img
+test -d Build || mkdir Build
+test -d $MOUNT || mkdir -p $MOUNT
+echo "Compiling source"
+cd Build
+cmake ../
+make
+echo "Generate image"
+cd $ROOT
+cp $GRUB $TARGET
+sudo mount $TARGET $MOUNT
+sudo cp $BUILD/kernel.elf $MOUNT/kernel
+sudo umount $MOUNT
+echo "Done"
