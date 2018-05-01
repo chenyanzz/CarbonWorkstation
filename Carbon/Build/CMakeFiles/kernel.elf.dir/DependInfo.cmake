b9d3e972505c736956f51f15819a6365994a35c9
c4droid <c4droid@foxmail.com>
2018-05-01 21:43:06 +0800

    Start to configure grub2

---
+++ Carbon/Build/CMakeFiles/kernel.elf.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/c4droid/CarbonWorkstation/Carbon/Bootloader/Bootloader.c" "/home/c4droid/CarbonWorkstation/Carbon/Build/CMakeFiles/kernel.elf.dir/Bootloader/Bootloader.c.o"
+  "/home/c4droid/CarbonWorkstation/Carbon/Interrupt/InterruptController.c" "/home/c4droid/CarbonWorkstation/Carbon/Build/CMakeFiles/kernel.elf.dir/Interrupt/InterruptController.c.o"
+  "/home/c4droid/CarbonWorkstation/Carbon/Interrupt/InterruptDescriptor.c" "/home/c4droid/CarbonWorkstation/Carbon/Build/CMakeFiles/kernel.elf.dir/Interrupt/InterruptDescriptor.c.o"
+  "/home/c4droid/CarbonWorkstation/Carbon/Interrupt/InterruptService.c" "/home/c4droid/CarbonWorkstation/Carbon/Build/CMakeFiles/kernel.elf.dir/Interrupt/InterruptService.c.o"
+  "/home/c4droid/CarbonWorkstation/Carbon/Kernel/Kernel.c" "/home/c4droid/CarbonWorkstation/Carbon/Build/CMakeFiles/kernel.elf.dir/Kernel/Kernel.c.o"
+  "/home/c4droid/CarbonWorkstation/Carbon/Library/CarbonELFDescriptor.c" "/home/c4droid/CarbonWorkstation/Carbon/Build/CMakeFiles/kernel.elf.dir/Library/CarbonELFDescriptor.c.o"
+  "/home/c4droid/CarbonWorkstation/Carbon/Library/CarbonKernelIO.c" "/home/c4droid/CarbonWorkstation/Carbon/Build/CMakeFiles/kernel.elf.dir/Library/CarbonKernelIO.c.o"
+  "/home/c4droid/CarbonWorkstation/Carbon/Runtime/Assert.c" "/home/c4droid/CarbonWorkstation/Carbon/Build/CMakeFiles/kernel.elf.dir/Runtime/Assert.c.o"
+  "/home/c4droid/CarbonWorkstation/Carbon/Runtime/KernelAssembly.c" "/home/c4droid/CarbonWorkstation/Carbon/Build/CMakeFiles/kernel.elf.dir/Runtime/KernelAssembly.c.o"
+  "/home/c4droid/CarbonWorkstation/Carbon/Runtime/String.c" "/home/c4droid/CarbonWorkstation/Carbon/Build/CMakeFiles/kernel.elf.dir/Runtime/String.c.o"
+  "/home/c4droid/CarbonWorkstation/Carbon/SegmentTable/GlobalDescriptor.c" "/home/c4droid/CarbonWorkstation/Carbon/Build/CMakeFiles/kernel.elf.dir/SegmentTable/GlobalDescriptor.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Carbon/Build/CMakeFiles/kernel.elf.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kernel.elf.dir/Bootloader/Bootloader.c.o"
+  "CMakeFiles/kernel.elf.dir/Interrupt/InterruptController.c.o"
+  "CMakeFiles/kernel.elf.dir/Interrupt/InterruptDescriptor.c.o"
+  "CMakeFiles/kernel.elf.dir/Interrupt/InterruptService.c.o"
+  "CMakeFiles/kernel.elf.dir/Kernel/Kernel.c.o"
+  "CMakeFiles/kernel.elf.dir/Library/CarbonELFDescriptor.c.o"
+  "CMakeFiles/kernel.elf.dir/Library/CarbonKernelIO.c.o"
+  "CMakeFiles/kernel.elf.dir/Runtime/Assert.c.o"
+  "CMakeFiles/kernel.elf.dir/Runtime/KernelAssembly.c.o"
+  "CMakeFiles/kernel.elf.dir/Runtime/String.c.o"
+  "CMakeFiles/kernel.elf.dir/SegmentTable/GlobalDescriptor.c.o"
+  "Build/kernel.elf.pdb"
+  "Build/kernel.elf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kernel.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
