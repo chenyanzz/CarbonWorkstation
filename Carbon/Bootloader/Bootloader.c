791862475b32b651603ebbe94ca0a8a58e0a6c46
c4droid <c4droid@foxmail.com>
2018-05-02 20:41:31 +0800

    Update the bootloader code,Using GRUB2 Bootloader standard

--- Carbon/Bootloader/Bootloader.c
+++ Carbon/Bootloader/Bootloader.c
@@ -2,28 +2,13 @@
 #include "../Include/CarbonDefine.h"
 #include "../Include/CarbonKernelIO.h"
 #include "../Include/KernelAssembly.h"
-typedef struct __multiboot_header__ {
-    uint32_t magic;
-    uint32_t flags;
-    uint32_t checksum;
-} MultibootHeader;
-MultibootHeader mbr __attribute__((section(".init.text"))) = {
-        .magic = 0x1BADB002,
-        .flags = 0x00000003,
-        .checksum = -(0x1BADB002 + 0x00000003),
+MultibootHeader master_boot_record __attribute__((section(".init.text"))) = {
+        MULTIBOOT_MAGIC,
+        MULTIBOOT_ARCHITECTURE,
+        sizeof(MultibootHeader),
+        0 - MULTIBOOT_MAGIC - MULTIBOOT_ARCHITECTURE - sizeof(MultibootHeader),
+        {0, 0, 8}
 };
-uint32_t magic;
-Multiboot *multiboot_ptr;
 void BootEntry(void) {
-    asm("movl %%eax,%0" : "=m"(magic));
-    if (magic != 0x2BADB002) {
-        printk("Error: Cannot loading kernel\n");
-        return;
-    }
-    asm("movl %%ebx,%0" : "=m"(multiboot_ptr));
-    clear();
-    cli();
-    printk("Booting...");
-    KernelEntry();
-    hlt();
+
 }
--- Carbon/Kernel/Kernel.c
+++ Carbon/Kernel/Kernel.c
@@ -1,38 +1,9 @@
-#include "../Include/Assert.h"
 #include "../Include/Bootloader.h"
-#include "../Include/CarbonDefine.h"
-#include "../Include/CarbonELFDescriptor.h"
-#include "../Include/CarbonKernelIO.h"
-#include "../Include/CarbonStdArg.h"
-#include "../Include/Descriptor.h"
-#include "../Include/Gate.h"
-#include "../Include/GlobalDescriptor.h"
-#include "../Include/InterruptController.h"
-#include "../Include/InterruptService.h"
-#include "../Include/KernelAssembly.h"
-#include "../Include/String.h"
-int memory_size;
-extern Multiboot *multiboot_ptr;
 extern uint32_t kernel_start[];
 extern uint32_t kernel_end[];
-GDT gdt_table = { 0 };
-Multiboot *global_multiboot_ptr = (void *)0;
 void KernelEntry() {
-    global_multiboot_ptr = multiboot_ptr;
-    memory_size = global_multiboot_ptr->mem_upper * 1024;
     KernelMain();
 }
 void KernelMain() {
-    InitDebug();
-    clear();
-    printk("Kernel is running...\n");
-    printk("In the memory:\n");
-    printk("Kernel in memory start: 0x%08X\n", kernel_start);
-    printk("Kernel in memory end: 0x%08X\n", kernel_end);
-    printk("Kernel in memory used: %d KB\n\n", (kernel_end - kernel_start) / 1024);
-    // init_memory_manager(memory_size,(uint32_t)kernel_start,(uint32_t)kernel_end);
-    InitGDT((uint32_t)&gdt_table);
-    InitIDT();
-    // init_kmemory();
-    sti();
+
 }
